7472aee2d25a5c26ea00e81d42b62710b35cfc7e
Jonathan Tsang <jonathantsang@users.noreply.github.com>
2019-07-29 11:03:44 -0700

    Add files via upload

--- camels.py
+++ camels.py
@@ -27,6 +27,9 @@ class Game():
 		
 		self.dicedone = {} # Key: camel id, Value: true or false for rolled yet
 
+		# Traps
+		self.traps = {} # Key: player id, Value: (+1 or -1, position in the board)
+
 		self.won = False
 		print(self.board)
 
@@ -45,6 +48,16 @@ class Game():
 			elif (parsed[1] == 1 or parsed[1] == 2 or parsed[1] == 3 or parsed[1] == 4 or parsed[1] == 5):
 				return False
 		return True
+
+	def CalculatePayoff(self, order, camelorder):
+		if camelorder <= 3:
+			return -1
+		elif camelorder == 2:
+			return 1
+		if order == 1:
+			return 5
+		else:
+			return 3
 
 	def Setup(self):
 		print("Players")
@@ -73,6 +86,7 @@ class Game():
 				# Player i+1
 				print("Player " + str(i) + "'s Turn:")
 
+				# Python 3
 				inp = input()
 				while not self.Valid(inp):
 					print("ERROR in command")
@@ -101,6 +115,7 @@ class Game():
 					self.dicedone[cameldice] = 1
 					
 					if len(self.dicedone) == self.camelcount:
+						print("End of Leg")
 						self.EndOfLeg()
 
 					for c in self.camelspos:
@@ -110,20 +125,34 @@ class Game():
 
 				# Place trap
 				if inp[0] == 'pt':
+					# This can fail due to not valid trap space
+					if (self.CheckPossible(inp)):
+						continue
+
+
 					pass
+
 				# Vote loser
 				if inp[0] == 'vl':
+					# This can fail if you don't own the card for voting winner/loser
+					if (self.CheckPossible(inp)):
+						continue
 					self.loserguesses.append((inp[1], i))
 					pass
 
 				# Vote winner
 				if inp[0] == 'vw':
+					# This can fail if you don't own the card for voting winner/loser
+					if (self.CheckPossible(inp)):
+						continue
 					self.winnerguesses.append((inp[1], i))
 					pass
 
 				# Vote camel
 				if inp[0] == 'vc':
-					
+					# This can fail if 2 players already voted for a camel
+					if (self.CheckPossible(inp)):
+						continue
 					pass
 
 				i += 1
@@ -198,10 +227,31 @@ class Game():
 	# Once all camels have moved
 	def EndOfLeg(self):
 		print("EndofLeg")
-		
-		# Tally bets
 
+		# Find the camel orders
+		camels = []
+		for camel in self.camelspos:
+			# (position, id)
+			camels.append((camel[1], camel[0]))
+		camels.sort()
 
+		print("CAMEL PLACES")
+		print(camels)
+
+		camelpos = {} # Key: Camel id, Value: Camel position relative to others 1,2,3,4,5
+		for i, camel in enumerate(camels):
+			camelpos[i] = camel[0]
+
+		# Update the scores
+		for bet in self.bets:
+			playerdid = bet
+			camelid = self.bets[bet][0]
+			orderofbet = bet[1]
+
+			payoff = self.CalculatePayoff(orderofbet, camelpos[camelid])
+			self.players[playerid] += payoff
+
+		self.Scores()
 
 		self.dicedone = {} # clear dice again
 
@@ -213,7 +263,33 @@ class Game():
 	# voting on current camel isn't exhausted
 	# placing a tile not beside another tile, or under current camels
 	def CheckPossible(self, inp):
-		pass
+		if inp[0] == 'pt':
+			# This can fail due to not valid trap space
+			placed = int(inp[1])
+
+			# If there is an adjacent trap, you cannot place it there
+			for trap in self.traps:
+				if 
+
+			pass
+
+		# Vote loser
+		if inp[0] == 'vl':
+			# This can fail if you don't own the card for voting winner/loser
+			
+			pass
+
+		# Vote winner
+		if inp[0] == 'vw':
+			# This can fail if you don't own the card for voting winner/loser
+			
+			pass
+
+		# Vote camel
+		if inp[0] == 'vc':
+			# This can fail if 2 players already voted for a camel
+
+			pass
 
 # Turn
 # Roll -> r
@@ -229,5 +305,6 @@ class Game():
 # First place, second, 3-5
 # first to vote: 5, 1, -1 
 # second to vote: 3, 1, -1
+# no third
 
 a = Game(20)
