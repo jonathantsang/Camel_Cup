da86b7ec7ae27226f66bd73d2c76671f8cb28bbd
Jonathan Tsang <jonathantsang@users.noreply.github.com>
2019-08-09 17:06:45 -0700

    Game should work
    no api yet

--- camels.py
+++ camels.py
@@ -54,6 +54,9 @@ class Game():
 			return -1
 		elif camelorder == 2:
 			return 1
+		# Order is 0st, 1st, 2nd, etc.
+		order += 1
+
 		if order == 1:
 			return 5
 		else:
@@ -81,18 +84,39 @@ class Game():
 		self.PrintBoard()
 
 		while(not self.won):
-			i = 0
+			i = 0 # current player turn
+
 			while(i < self.numplayers):
 				# Player i+1
 				print("Player " + str(i) + "'s Turn:")
+				print("DEBUG")
+				print("d -> debug")
+				print("s -> human readable scores")
+				print("m -> parseable data for AI")
 
 				# Python 3
 				inp = input()
+
+				# DEBUG
+				if inp[0] == 'd':
+					self.Debug()
+					continue
+				elif inp[0] == 's':
+					self.Scores()
+					continue
+				elif inp[0] == 'm':
+					self.Data()
+
 				while not self.Valid(inp):
 					print("ERROR in command")
 					print("Valid Turn Commands: \nRoll -> r\nPlace Trap -> pt 1-size\nVote Loser -> vl 1,2,3,4,5\nVote Winner -> vw 1,2,3,4,5\nVote on Camel -> vc 1,2,3,4,5")
 					inp = input()
 				print("command: " + inp)
+
+				# Could fail, loop again
+				if (not self.CheckPossible(inp, i)):
+					print("Move is not possible. Please try again.")
+					continue
 
 				# Perform the action
 				inp = inp.split()
@@ -124,36 +148,38 @@ class Game():
 							self.won = True
 
 				# Place trap
-				if inp[0] == 'pt':
+				elif inp[0] == 'pt':
 					# This can fail due to not valid trap space
-					if (self.CheckPossible(inp)):
-						continue
-
-
-					pass
+					self.traps[i] = (int(inp[1]), int(inp[2]))					
 
 				# Vote loser
-				if inp[0] == 'vl':
+				elif inp[0] == 'vl':
+					camelid = int(self.camelarray.find(inp[1]))
 					# This can fail if you don't own the card for voting winner/loser
-					if (self.CheckPossible(inp)):
-						continue
-					self.loserguesses.append((inp[1], i))
-					pass
+					self.loserguesses.append((camelid, i))
 
 				# Vote winner
-				if inp[0] == 'vw':
+				elif inp[0] == 'vw':
+					int(self.camelarray.index(inp[1]))
 					# This can fail if you don't own the card for voting winner/loser
-					if (self.CheckPossible(inp)):
-						continue
-					self.winnerguesses.append((inp[1], i))
-					pass
+					self.winnerguesses.append((camelid, i))
 
 				# Vote camel
-				if inp[0] == 'vc':
+				elif inp[0] == 'vc':
 					# This can fail if 2 players already voted for a camel
-					if (self.CheckPossible(inp)):
-						continue
-					pass
+					if i not in self.bets:
+						self.bets[i] = []
+
+					# Find 1st, 2nd, 3rd
+					camelid = int(self.camelarray.index(inp[1]))
+					amt = 0
+
+					for player_bets in self.bets:
+						for bet in self.bets[player_bets]:
+							if bet[0] == camelid:
+								amt += 1
+
+					self.bets[i].append((camelid, amt))
 
 				i += 1
 
@@ -214,15 +240,69 @@ class Game():
 		# Move to new position
 		self.board[self.camelspos[camel]].extend(camelcomponent)
 
+		# Check if it is a trap and move accordingly
+		for player in self.traps:
+			val = self.traps[player][0]
+			position = self.traps[player][1]
+
+			if position == self.camelspos[camel]: 
+				# Credit player who placed the trap
+				self.players[player] += 1
+
+				# +1
+				if val == 1:
+					for c in camelcomponent:
+						self.camelspos[c] += 1
+					# Move to new position, appending to the ones there
+					self.board[self.camelspos[camel]].extend(camelcomponent)
+				elif val == -1:
+					for c in camelcomponent:
+						self.camelspos[c] -= 1
+					# Move to new position, placing under the previous if there
+					self.board[self.camelspos[camel]] = camelcomponent.extend(self.board[self.camelspos[camel]])
+				break # No traps on traps
+
 	# Prints position of game for AIs (can only see current player's information)
 	def Position(self, player):
-		pass
+		print("Information for player" + str(player))
+		print("Player " + str(player) + "has " + str(self.players[player]) + " points")
+
+	# Debug for state of game currently
+	def Debug(self):
+		print("camelcount")
+		print(self.camelcount)
+		print("players")
+		print(self.players)
+		print("boardsize")
+		print(self.boardsize)
+		print("board")
+		print(self.board)
+		print("camelarray")
+		print(self.camelarray)
+		print("camelspos")
+		print(self.camelspos)
+		print("bets")
+		print(self.bets)
+		print("winnerguesses")
+		print(self.winnerguesses)
+		print("loserguesses")
+		print(self.loserguesses)
+		print("dicedone")
+		print(self.dicedone)
+		print("traps")
+		print(self.traps)
+		print("won")
+		print(self.won)
 
 	# Scores of people human-readable
 	def Scores(self):
 		for player in self.players:
-			print("Player: " + player)
+			print("Player: " + str(player))
 			print("Score: " + self.players[player])
+
+	# Data for computers
+	def Data(self):
+		pass
 
 	# Once all camels have moved
 	def EndOfLeg(self):
@@ -259,41 +339,59 @@ class Game():
 	def EndOfGame(self):
 		print("EndofGame")
 		pass
+
 	# Check if move is possible:
-	# voting on current camel isn't exhausted
+	# voting on current camel isn't exhausted (2+ players)
 	# placing a tile not beside another tile, or under current camels
-	def CheckPossible(self, inp):
+	# if you don't own card to vote on winner/loser
+	def CheckPossible(self, inp, playerid):
 		if inp[0] == 'pt':
 			# This can fail due to not valid trap space
 			placed = int(inp[1])
 
 			# If there is an adjacent trap, you cannot place it there
 			for trap in self.traps:
-				if 
+				if (trap[1] - 1 == placed) or (trap[1] + 1 == placed):
+					print("Invalid trap spot since it is adjacent to a trap on " + str(trap[1]))
+					return False
 
-			pass
+		# Vote loser or Vote winner
+		elif inp[0] == 'vl' or inp[0] == 'vw':
+			# camel id to attempt to guess
+			camelid = int(self.camelarray.index(inp[1])) # Search for id
 
-		# Vote loser
-		if inp[0] == 'vl':
 			# This can fail if you don't own the card for voting winner/loser
-			
-			pass
+			for guess in self.winnerguesses:
+				if guess[1] == playerid and guess[0] == camelid:
+					print("Player has already bet on a loser/winner with camelid")
+					return False
 
-		# Vote winner
-		if inp[0] == 'vw':
-			# This can fail if you don't own the card for voting winner/loser
-			
-			pass
+			for guess in self.loserguesses:
+				if guess[1] == playerid and guess[0] == camelid:
+					print("Player has already bet on a loser/winner with camelid")
+					return False
 
 		# Vote camel
-		if inp[0] == 'vc':
+		elif inp[0] == 'vc':
 			# This can fail if 2 players already voted for a camel
+			camelid = int(self.camelarray.index(inp[1])) # Search for id
+			amt = 0
 
-			pass
+			for player_bets in self.bets:
+				for bet in player_bets[1]:
+					if bet[0] == camelid:
+						amt += 1
+
+			if amt == 2:
+				print("Camel already has 2 bettors")
+				return False
+
+		return True
 
 # Turn
 # Roll -> r
-# place trap -> pt 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 (still valid spot check)
+# vote on camel -> vc r, b, g, w, o
+# place trap -> pt 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 (still valid spot check) 1 or -1
 # vote loser -> vl 1,2,3,4,5
 # vote winner -> vw 1,2,3,4,5
 # Winners/Losers, reverse order
